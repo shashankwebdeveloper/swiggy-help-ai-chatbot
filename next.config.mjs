52e57283b97ac35bb03e662ff32a2336ac0ba80a
shashankwebdeveloper <shashankmscs25@gmail.com>
2024-06-22 19:48:22 +0530

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
   env: {
     DIALOGFLOW_PROJECT_ID: process.env.DIALOGFLOW_PROJECT_ID,
     DIALOGFLOW_CLIENT_EMAIL: process.env.DIALOGFLOW_CLIENT_EMAIL,
-    DIALOGFLOW_PRIVATE_KEY: process.env.DIALOGFLOW_PRIVATE_KEY
+    DIALOGFLOW_PRIVATE_KEY: process.env.DIALOGFLOW_PRIVATE_KEY.replace(/\\n/g, '\n'),
   },
 };
 
