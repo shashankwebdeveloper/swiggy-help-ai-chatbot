7785bdd62301ff8eb82a73c6089baf350fe8d53b
shashankwebdeveloper <shashankmscs25@gmail.com>
2024-06-22 19:00:27 +0530

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 
-const dialogflowPrivateKey = process.env.DIALOGFLOW_PRIVATE_KEY
-  ? process.env.DIALOGFLOW_PRIVATE_KEY.replace(/\\n/g, '\n')
-  : '';
+// const dialogflowPrivateKey = process.env.DIALOGFLOW_PRIVATE_KEY
+//   ? process.env.DIALOGFLOW_PRIVATE_KEY.replace(/\\n/g, '\n')
+//   : '';
 const nextConfig = {
   reactStrictMode: true,
   env: {
     DIALOGFLOW_PROJECT_ID: process.env.DIALOGFLOW_PROJECT_ID,
     DIALOGFLOW_CLIENT_EMAIL: process.env.DIALOGFLOW_CLIENT_EMAIL,
-    DIALOGFLOW_PRIVATE_KEY: dialogflowPrivateKey
+    DIALOGFLOW_PRIVATE_KEY: process.env.DIALOGFLOW_PRIVATE_KEY
   },
 };
 
